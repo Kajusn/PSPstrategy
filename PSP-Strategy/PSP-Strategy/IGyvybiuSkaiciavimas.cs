95d8f55999631b502653dee924370ea055457537
kajusn <kajusn@gmail.com>
2019-10-16 12:14:20 +0300

    Added strategy interfaces

---
+++ PSP-Strategy/PSP-Strategy/IGyvybiuSkaiciavimas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSP_Strategy
+{
+    interface IGyvybiuSkaiciavimas
+    {
+        double veikejoGyvybes(int gyvybes, double sarvai, int lygis);
+        double papildomosGyvybes(string klase, int gyvybes, string kilme);
+    }
+}
---
+++ PSP-Strategy/PSP-Strategy/IPuolimoStrategija.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSP_Strategy
+{
+    interface IPuolimoStrategija
+    {
+        String puolimoStrategija(double sarvai, string klase);
+        float zalosKeitimas(int lygis, int gyvybes);
+    }
+}
