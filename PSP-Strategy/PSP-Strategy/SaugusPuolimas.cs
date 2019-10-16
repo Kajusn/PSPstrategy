9c8acaabaf6b159ada4451278d70a4970f9ebe55
kajusn <kajusn@gmail.com>
2019-10-16 13:22:18 +0300

    Added Attack implementation classes, no algorithms

---
+++ PSP-Strategy/PSP-Strategy/SaugusPuolimas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSP_Strategy
+{
+    public class SaugusPuolimas : IPuolimoStrategija
+    {
+        public string puolimoStrategija(double sarvai, string klase)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float zalosKeitimas(int lygis, int gyvybes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
